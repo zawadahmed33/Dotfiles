b1cf56930ce706b35f26e890672dece66f9cba2f
Zawad Ahmed <contactzawad@gmail.com>
2025-01-04 22:24:14 +0600

    first commit in fancy branch

---
+++ Fancy/qtile/autostart.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+picom &
+dunst &
+~/.fehbg &
+numlockx &
---
+++ Fancy/rofi/quicklinks/quicklinks.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+chosen=$(printf "\n\n\n" | rofi -dmenu -i -theme-str '@import "quicklinks.rasi"')
+
+case "$chosen" in
+	"") chromium https://www.google.com ;;
+	"") chromium https://www.youtube.com ;;
+	"") chromium https://www.reddit.com ;;
+	"") chromium https://www.github.com ;;
+	*) exit 1 ;;
+esac
